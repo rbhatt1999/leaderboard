87282176684511c785bf364725030e4bbf44a6fa
rbhatt199924 <rbhatt199924@gmail.com>
2022-12-20 14:38:38 +0530

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -3,22 +3,21 @@ import display from './modules/display.js';
 
 const scoreList = [
   {
-    name:'rohit',
-    score:45,
+    name: 'rohit',
+    score: 45,
   },
   {
-    name:'Rajesh',
-    score:70,
+    name: 'Rajesh',
+    score: 70,
   },
   {
-    name:'Montu',
-    score:65,
+    name: 'Montu',
+    score: 65,
   },
   {
-    name:'Gopi',
-    score:48,
+    name: 'Gopi',
+    score: 48,
   },
 ];
 
 display(scoreList);
-
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,8 +1,8 @@
 export default (arr) => {
   const scoreListContainer = document.querySelector('.score-list');
-  arr.forEach((item,index) => {
+  arr.forEach((item) => {
     const li = document.createElement('li');
-    li.innerHTML = `${item.name}: ${item.score}`
-    scoreListContainer.appendChild(li);    
+    li.innerHTML = `${item.name}: ${item.score}`;
+    scoreListContainer.appendChild(li);
   });
-}
+};
