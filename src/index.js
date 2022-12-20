1aec77b21340456bdc190304f59ff164d2d58839
rbhatt199924 <rbhatt199924@gmail.com>
2022-12-20 14:34:01 +0530

    add manual scorelist array

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './index.css';
+import display from './modules/display.js';
+
+const scoreList = [
+  {
+    name:'rohit',
+    score:45,
+  },
+  {
+    name:'Rajesh',
+    score:70,
+  },
+  {
+    name:'Montu',
+    score:65,
+  },
+  {
+    name:'Gopi',
+    score:48,
+  },
+];
+
+display(scoreList);
+
